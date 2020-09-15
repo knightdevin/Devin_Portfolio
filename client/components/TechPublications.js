6484ff68ca37f50dbc8dd466a3b922bdd69cd771
Devin Knight <knightdevin@hotmail.com>
2020-09-15 17:56:53 -0400

    added hover css to tech blog article & altered external link to open new window when clicked

--- client/components/TechPublications.js
+++ client/components/TechPublications.js
@@ -10,25 +10,32 @@ const TechPublications = () => {
       <br />
       <h1 className="contactMe">Tech Blogs</h1>
 
-      <div className="articleBox">
-        <h2>A Better Way To Fill Your Database Tables With Random Data</h2>
-        <h4>
-          Take the headache out of managing a seed file with a simple JavaScript
-          library
-        </h4>
-        <a href="https://blog.usejournal.com/a-better-way-to-fill-your-database-tables-with-random-data-714da8afd061">
+      <a
+        className="react-router__link"
+        href="https://blog.usejournal.com/a-better-way-to-fill-your-database-tables-with-random-data-714da8afd061"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <div className="articleBox">
+          <h2>A Better Way To Fill Your Database Tables With Random Data</h2>
+          <h4>
+            Take the headache out of managing a seed file with a simple
+            JavaScript library
+          </h4>
+          {/* <a href="https://blog.usejournal.com/a-better-way-to-fill-your-database-tables-with-random-data-714da8afd061"> */}
           <img
             src="/photos/ArticledatabasePhoto.png"
             className="hobbyShopperPhoto"
           />
-        </a>
-        <p>
-          So you’re developing an idea for a new app or website. Maybe you and
-          your colleagues just wrapped up a big planning session. Everyone knows
-          what the app is, who will be using it and what it will be used for.
-          High-fives all around, right?
-        </p>
-      </div>
+          {/* </a> */}
+          <p>
+            So you’re developing an idea for a new app or website. Maybe you and
+            your colleagues just wrapped up a big planning session. Everyone
+            knows what the app is, who will be using it and what it will be used
+            for. High-fives all around, right?
+          </p>
+        </div>
+      </a>
     </div>
   )
 }

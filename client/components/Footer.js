db8ba762577bdc2f4ec68b450b0711801c34f0b7
Devin Knight <knightdevin@hotmail.com>
2020-09-21 17:57:43 -0400

    added contact icons in footer & altered styling

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,15 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
 
+// ====================== STYLED COMPONENTS ======================
+
 const FooterStyles = styled.p`
   text-align: center;
   font-family: 'Trebuchet MS', Helvetica, sans-serif;
   font-size: large;
+  margin-top: 0px;
 `
+
+const FooterLogo = styled.img`
+  height: 40px;
+`
+
+const FooterLogosContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 20%;
+  align-items: center;
+`
+
+// ===============================================================
 
 const Footer = () => {
   return (
     <div className="footer">
+      <FooterLogosContainer>
+        <a
+          href="https://www.linkedin.com/in/devinknight/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FooterLogo src="logos/linkedin-logo-3.png" />
+        </a>
+        <a
+          href="https://github.com/knightdevin"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FooterLogo src="/logos/github - 25231.png" />
+        </a>
+        <a href="mailto:knightdevin@hotmail.com">
+          <FooterLogo src="/logos/clipart316850.png" />
+        </a>
+      </FooterLogosContainer>
       <FooterStyles>Created by Devin Knight in 2020</FooterStyles>
     </div>
   )

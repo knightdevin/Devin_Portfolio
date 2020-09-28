9a6bd202083df8beb90f77db6a3c70147d650898
Devin Knight <knightdevin@hotmail.com>
2020-09-28 11:56:58 -0400

    adjusted code button on landing page

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import {Helmet} from 'react-helmet'
+import {NavLink, Link} from 'react-router-dom'
 
 // ====================== STYLED COMPONENTS ======================
 const TextOnPhoto = styled.h2`
@@ -21,20 +22,50 @@ const LandingContainer = styled.div`
 `
 
 const BackgroundOverlayImg = styled.img`
-  height: 1030px;
-  width: 100%;
+  height: 911px;
+  width: 77%;
   opacity: 0.05;
   position: absolute;
 `
 const BackgroundImg = styled.img`
-  height: 100%;
-  width: 100%;
+  height: 911px;
+  width: 77%;
   display: flex;
   // flex-grow;
   position: relative;
   top: 0;
   left: 0;
   box-shadow: 0 0 8px 8px white inset;
+`
+
+const StyledButton = styled(Link)`
+  // background-color: red;
+  width: 5%;
+  height: 13%;
+  position: absolute;
+  margin-right: 24px;
+  margin: 30%;
+  margin: 539px 0 0 -285px;
+  transform: rotate(65deg) skew(-9deg, 0deg);
+  opacity: 0.4;
+  transition: 0.5s all ease-out;
+
+  :hover {
+    transform: rotate(65deg) skew(-9deg, 0deg) translateY(-3px) scale(1.01);
+    cursor: pointer;
+  }
+`
+
+const ButtonImg = styled.img`
+  // transform: rotate(-76deg) skew(6deg, -16deg);
+  opacity: 0.95;
+
+  height: 125%;
+  transform: rotate(-77deg) skewX(14deg) skewY(-27deg);
+  margin-top: -30px;
+  /* margin-top: -234px; */
+  42px: ;
+  width: 145%;
 `
 
 // ===============================================================
@@ -48,6 +79,10 @@ const LandingPage = () => {
       {/* <TextOnPhoto>A Developer with a Different Background</TextOnPhoto> */}
       <BackgroundImg src="/photos/extra photos/photos/501_3872.JPG" />
       <BackgroundOverlayImg src="/photos/extra photos/learn-code-e1455713167295.jpg" />
+      <StyledButton to="/projects">
+        {/* <ButtonImg src="/photos/extra photos/code_img.jpg" /> */}
+        <ButtonImg src="/photos/extra photos/photos/Helper_Func_Code_Image.png" />
+      </StyledButton>
     </LandingContainer>
   )
 }

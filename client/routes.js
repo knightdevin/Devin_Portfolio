153f0bafe5c9a4af2f3aba494c0494907face831
Devin Knight <knightdevin@hotmail.com>
2020-09-30 10:40:29 -0400

    changed from BrowserRouter to HashRouter as suggested for potential github Pages fix

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import {
   LandingPage,
@@ -15,16 +15,20 @@ import {
 export default class Routes extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/education-experience" component={ExpAndEdu} />
-        <Route path="/publications" component={Publications} />
-        <Route path="/past-life" component={PastLife} />
-        <Route path="/contact" component={Contact} />
-        <Route exact path="/" component={LandingPage} />
-      </Switch>
+      <div>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/skills" component={Skills} />
+            <Route path="/education-experience" component={ExpAndEdu} />
+            <Route path="/publications" component={Publications} />
+            <Route path="/past-life" component={PastLife} />
+            <Route path="/contact" component={Contact} />
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </HashRouter>
+      </div>
     )
   }
 }
